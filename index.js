fc850b41a99600a8115b5604e9342931f29ed54f
raseeksht <raseeksht1@gmail.com>
2023-11-30 13:35:45 +0545

    added few details about api at /

--- index.js
+++ index.js
@@ -23,8 +23,35 @@ app.use(express.json())
 
 app.use("/api",api)
 
+const html = `<h1>Intuji Backend Assessment</h1>
+<h3>Blog API</h3>
+<div>Api documentation: <a href="https://documenter.getpostman.com/view/26416014/2s9YeHZW6U#648236f5-a42b-40fe-84c8-5ac55351b201" target="_blank">https://documenter.getpostman.com/view/26416014/2s9YeHZW6U#648236f5-a42b-40fe-84c8-5ac55351b201</a></div>
+<div>Github Source Code: <a href="https://github.com/raseeksht/Intuji-Backend-Assessment" target="_blank">https://github.com/raseeksht/Intuji-Backend-Assessment</a></div>
+
+<h4>Api Endpoints</h4>
+<ol>
+    <li> Blogs 
+        <ul>
+            <li> (get) /api/blogs </li>
+            <li> (get) /api/blogs/&lt;blogId&gt;</li>
+            <li> (post) /api/blogs </li>
+            <li> (put) /api/blogs/&lt;blogId&gt; </li>
+        </ul>
+    </li>
+    <li> Categories
+        <ul>
+            <li> (get) /api/categories </li>
+            <li> (post) /api/categories </li>
+            <li> (delete) /api/categories/&lt;categoryId&gt; </li>
+        </ul>
+    </li>
+
+</ol>
+
+`
+
 app.get("/",(req,resp)=>{
-    resp.json({"message":"okay"})
+    resp.send(html)
 })
 
 
