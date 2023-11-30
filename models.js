1745ef1fba910a08e65479dd6b84a69b0ee18ac8
raseeksht <raseeksht1@gmail.com>
2023-11-30 12:40:41 +0545

    on delete cascade implemented while deleting a category

--- models.js
+++ models.js
@@ -17,6 +17,24 @@ const categorySchema = new mongoose.Schema({
     categoryName:String
 })
 
+// function: on delete cascade behaviour
+// if a category is deleted, all the blogs that have that specific category will be deleted as well
+categorySchema.pre("deleteOne",async function (next){
+    refId = this.getQuery()._id
+    // console.log("this refid will be deleted",refId)
+    // get all the blogs whose category match the category to be deleted
+    try{
+        const result = await blogModel.deleteMany({category:refId})
+        // console.log("the resulting blogs are s",result)
+        next()
+    }
+    catch(err){
+        console.log("err occured",err)
+        next(err)
+    }
+})
+
+
 const blogModel = mongoose.model("blog",blogSchema)
 const categoryModel = mongoose.model("category",categorySchema)
 
